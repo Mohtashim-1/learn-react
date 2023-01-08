7b6bd69b5947960ed4c41032a4da0b2978581197
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2023-01-08 18:50:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Student from "./Components/Student";
+
+function App() {
+  return (
+    <div className="App">
+<Student name="mohtashim"/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Student = (props) => {
+  return (
+
+
+<h1>{props.name}</h1>
+
+
+  )
+}
+
+export default Student
